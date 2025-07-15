f07fc56173bfc9e9535848413ded39059f2b96fa
ingJuanArenas <juanarenasing@gmail.com>
2025-07-15 10:08:09 -0500

    initializing proyect...

---
+++ src/main/java/com/restaurant/restaurantmanagment/RestaurantmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurantmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantmanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurant/restaurantmanagment/RestaurantmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.restaurantmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
